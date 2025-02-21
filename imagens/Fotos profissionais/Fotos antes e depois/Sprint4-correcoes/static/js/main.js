1be362dbf5561923c8f805e47310405584443e78
Renatosilva2025 <srnato@hotmail.com>
2025-02-21 05:09:28 -0300

    Delete imagens/Fotos profissionais/Fotos antes e depois/Sprint4-correcoes directory

--- imagens/Fotos profissionais/Fotos antes e depois/Sprint4-correcoes/static/js/main.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-    const menu = document.querySelector('.ul');
-    menu.classList.add('active');
-}
-
-function closeMenu() {
-    const menu = document.querySelector('.ul');
-    menu.classList.remove('active');
-}
